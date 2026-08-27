b7ee2bce2a368f168b4d8d50e73cc10cc088eff6
Ontrip <ontrip.ai@gmail.com>
2026-08-27 23:27:58 +0300

    feat(auth): update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://va.vercel-scripts.com",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://va.vercel-scripts.com https://challenges.cloudflare.com",
+      "frame-src 'self' https://challenges.cloudflare.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com data:",
       "img-src 'self' data: blob: https://images.unsplash.com",
